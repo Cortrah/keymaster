ce9bab8df4ee27c12dfae52214f586ad1db01bd8
cortrah <cort@cortwinters.net>
2019-07-14 01:32:44 -0400

    Get rid of any tests that aren't real

--- tests/unit/user-item.spec.js
+++
@@ -1,9 +0,0 @@
-import {shallowMount} from '@vue/test-utils'
-import UserItem from '@/components/UserItem.vue'
-
-describe('UserItem', () => {
-
-    it('Renders a UserItem and allows one to archive it or add Permissions to it', () => {
-        expect(true).toBe(true);
-    });
-});
--- tests/unit/user-list.spec.js
+++
@@ -1,9 +0,0 @@
-import {shallowMount} from '@vue/test-utils'
-import UserList from '@/components/UserList.vue'
-
-describe('UserList', () => {
-
-    it('Renders a list of UserItems', () => {
-        expect(true).toBe(true);
-    });
-});
